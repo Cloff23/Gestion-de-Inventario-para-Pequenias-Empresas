e3da70ec13e4c6a61a87f96454748c5307674013
Manuu201 <manuel.vargasv@usm.cl>
2025-03-27 19:59:29 -0300

    Primera version del programa

---
+++ models.py
@@ -0,0 +1,7 @@
+class Producto:
+    def __init__(self, nombre, descripcion, cantidad, precio, categoria):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.cantidad = cantidad
+        self.precio = precio
+        self.categoria = categoria
